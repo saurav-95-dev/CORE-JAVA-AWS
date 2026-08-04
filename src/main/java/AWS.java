f5893b0fc2f17389d63287b952e02266507989ab
Saurabh <devsaurabh086@gmail.com>
2026-08-04 23:46:08 +0530

    IAM and AWS CLI

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -41,7 +41,7 @@ Rekognition(Software as a service).
 ### IAM AND AWS CLI :
 ->IAM - Identity and Access Management, Global Service.
 ->Group in IAM can contain users and not other groups.
-->Users don't necessarily need  to belong to a group.
+->Users don't necessarily need to belong to a group.
 ->A user can belong to a multiple group.
 ->We create users and groups to give them permission to use AWS service called IAM permission.
 ->Users and Groups can be assigned JSON documents called policies.

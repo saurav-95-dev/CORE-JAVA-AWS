93f73a23631cd51c51e4541adaa8b958182672e3
Saurabh <devsaurabh086@gmail.com>
2026-08-10 01:56:09 +0530

    IAM - Users and Groups

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -11,8 +11,14 @@ a)AWS Regions :
 →A region is basically a cluster of data centers .
 →Most AWS services are region scoped .
 
-Question : How to choose an AWS region to deply your application :
+Question : How to choose an AWS region to deploy your application :
 Factors affecting - Compliance with the govt , Proximity to customers (reduced latency) , Available service within a region , Pricing  (varies from region to region).
+->Not all services are available in all region .
+
+#INTRODUCTION TO IAM SERVICE:
+->Group in IAM can only contain users and not any other group.
+->A user may or may not belong to a group.
+->A user can belong to multiple group.
 
 */
 

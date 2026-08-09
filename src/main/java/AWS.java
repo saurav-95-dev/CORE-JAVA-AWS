24bef6605d1c75d0bd9bf1c841be9eb22e409f5b
Saurabh <devsaurabh086@gmail.com>
2026-08-09 23:04:17 +0530

    Concept of AWS Regions

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -1,8 +1,19 @@
 //ALL About AWS : PHASE-1:
 //----------------------------------------------------------------------------
 /*
-Some common usecases of AWS :
+# Some common use-cases of AWS :
 ->Enterprise IT, Big data analytics, Web hosting, BE for mobile and social apps, Backup and storage, Gaming servers
+
+# AWS Global Infrastructure:
+-> 4 Components - AWS Regions, AWS Availability Zones, AWS Data centers, AWS edge location/ppp.
+a)AWS Regions :
+→AWS Regions are all around the globe.
+→A region is basically a cluster of data centers .
+→Most AWS services are region scoped .
+
+Question : How to choose an AWS region to deply your application :
+Factors affecting - Compliance with the govt , Proximity to customers (reduced latency) , Available service within a region , Pricing  (varies from region to region).
+
 */
 
 

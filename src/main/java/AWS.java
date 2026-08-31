794c008fcfd2f7552a236416413cf7cea6fab05e
Saurabh <devsaurabh086@gmail.com>
2026-09-01 00:00:19 +0530

    Availability Zones and data centers

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -10,8 +10,16 @@ Create a Backend for Mobile and Social Apps, also we can have our gaming servers
 ->AWS Data Centers
 ->AWS Edge locations/ppp.
 
-3) Factors affecting aws region selection:
+3) a)Factors affecting aws region selection:
 -> Compliance, Proximity to users (reduced latency), Not all regions have all the services, Pricing.
 
+b)Availability Zones & Data Centers :
+→Each region consist of certain number of availablity zones (min = 3 , max = 6).
+→Each availability zones has - one or more decrete data centers (Isolated from each other from disaster).
+→Each data centers of availability zones are connected via high bandwidth , ultra low latency network.
+
+c)AWS Points of Presence :
+→Amazon has 400+ POP in 90+ cities accross  40+ countries.
+→Helps to deliver content to end users with lowest latency possible.
 
 */

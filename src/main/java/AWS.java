a3b95ce881b2949185fa4325ebe6751718e05456
Saurabh <devsaurabh086@gmail.com>
2026-08-31 23:46:46 +0530

    AWS Global Infrastructure Components

---
+++ src/main/java/AWS.java
@@ -0,0 +1,12 @@
+/*
+1)Some Common use-cases of AWS Cloud :
+Enterprise IT, Backup & Storage, Big Data Analytics, Web hosting,
+Create a Backend for Mobile and Social Apps, also we can have our gaming servers running on cloud.
+
+2)AWS Global infrastructure components :
+->AWS regions
+->Availability Zones.
+->AWS Data Centers
+->AWS Edge locations/ppp.
+
+*/

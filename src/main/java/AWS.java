8565ee65636d1479c0b9dfa9f2832a0fb31e94b3
Saurabh <devsaurabh086@gmail.com>
2026-08-31 23:56:19 +0530

    Factors affecting AWS Region Selection

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -1,4 +1,5 @@
 /*
+
 1)Some Common use-cases of AWS Cloud :
 Enterprise IT, Backup & Storage, Big Data Analytics, Web hosting,
 Create a Backend for Mobile and Social Apps, also we can have our gaming servers running on cloud.
@@ -8,5 +9,9 @@ Create a Backend for Mobile and Social Apps, also we can have our gaming servers
 ->Availability Zones.
 ->AWS Data Centers
 ->AWS Edge locations/ppp.
+
+3) Factors affecting aws region selection:
+-> Compliance, Proximity to users (reduced latency), Not all regions have all the services, Pricing.
+
 
 */
